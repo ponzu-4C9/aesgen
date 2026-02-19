fc7a81688615231b42cb85af9071a53657eb8f9f
ponzu-4C9 <baijingseiya@gmail.com>
2026-02-20 00:53:24 +0900

    初期値を空文字からサンプルデータに変更

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Viewexample from "@/app/components/Viewexample";
 
 
 export default function MyApp() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState(`{"words": [{"word": "follow","meaning": "～の後に続く、～に従う","examples": [{"example": "Please follow the instructions carefully.","meaning": "指示を注意深く従ってください。"}]},{"word": "consider","meaning": "考慮する、検討する","examples": [{"example": "I will consider your suggestion before making a decision.","meaning": "決定を下す前にあなたの提案を検討します。"}]}]}`);
   return (
     <div>
       <h1 className="daimei">Automatic Example Sentence GENerator</h1>
